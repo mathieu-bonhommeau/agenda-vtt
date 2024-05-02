e6924902b5da838477aa9344fb7ee0afd44b0c88
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2024-05-02 09:14:56 +0200

    wip: build datepickers for filter components with error management

--- client/src/app/_common/client/components/form/date-range-picker.tsx
+++ client/src/app/_common/client/components/form/date-range-picker.tsx
@@ -1,4 +1,5 @@
-import { DateRangePicker } from '@mui/x-date-pickers-pro'
+import { Alert, Box } from '@mui/material'
+import { DatePicker } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { Dayjs } from 'dayjs'
@@ -13,14 +14,45 @@ export type InputDatePickerProps = {
 }
 
 export function InputDateRangePicker({ startDateLabel, endDateLabel, locale }: InputDatePickerProps) {
-    const [dateRange, setDateRange] = useState<Array<Dayjs | null>>([])
+    const [startDate, setStartDate] = useState<Dayjs | null>(null)
+    const [endDate, setEndDate] = useState<Dayjs | null>(null)
+    const [error, setError] = useState<string | null>(null)
+
+    const handleStartDate = (date: Dayjs | null) => {
+        if (endDate && date?.isAfter(endDate)) {
+            setError('La date de début doit être inférieur à la date de fin')
+            return
+        }
+        setStartDate(date)
+        setError(null)
+    }
+
+    const handleEndDate = (date: Dayjs | null) => {
+        if (startDate && date?.isBefore(startDate)) {
+            setError('La date de fin doit être supérieur à la date de début')
+            return
+        }
+        setEndDate(date)
+        setError(null)
+    }
 
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={locale}>
-            <DateRangePicker
-                localeText={{ start: startDateLabel, end: endDateLabel }}
-                onChange={(e) => setDateRange(e)}
-            />
+            <Box sx={{ display: 'flex', gap: 2, my: 1 }}>
+                <DatePicker
+                    label={startDateLabel}
+                    onChange={(date) => handleStartDate(date)}
+                    disablePast
+                    slotProps={{ textField: { error: !!error } }}
+                />
+                <DatePicker
+                    label={endDateLabel}
+                    onChange={(date) => handleEndDate(date)}
+                    disablePast
+                    slotProps={{ textField: { error: !!error } }}
+                />
+            </Box>
+            {error && <Alert severity="error">{error}</Alert>}
         </LocalizationProvider>
     )
 }
