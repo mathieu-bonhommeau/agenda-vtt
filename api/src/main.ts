9175a0cf41a6f9d73f7f82424708311426ef4eb6
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2024-06-21 16:29:35 +0000

    feat: setup ci/cd

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.NODE_PORT || 5000, () =>
+    console.log('Listen : http://localhost:5000'),
+  );
 }
-bootstrap();
+bootstrap().then();
