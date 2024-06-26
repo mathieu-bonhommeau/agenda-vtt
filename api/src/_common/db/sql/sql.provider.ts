9a3bf403c0fe953905c16a4437eaf3336563cbe2
Mathieu Bonhommeau <m.bonhommeau@unlimitedpix.com>
2024-06-26 19:28:40 +0200

    wip: setup database

---
+++ api/src/_common/db/sql/sql.provider.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm'
+import { config } from 'dotenv'
+config()
+
+export const SqlProvider = {
+    provide: 'SQL',
+    useFactory: async () => {
+        const pg = new DataSource({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST || 'localhost',
+            port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+            username: process.env.POSTGRES_USER || 'postgres',
+            password: process.env.POSTGRES_PASSWORD || 'root',
+            database: process.env.POSTGRESS_DB || 'utagawa-agenda-db',
+        })
+    },
+}
